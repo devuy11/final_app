1efee051292ba33bb31693d308adf6010081cba5
LENOVO <devu@ubuntu.(none)>
2014-05-13 12:08:34 +0530

    second-last-commit

--- app/helpers/static_pages_helper.rb
+++ app/helpers/static_pages_helper.rb
@@ -1,2 +1,6 @@
 module StaticPagesHelper
+	require 'emoticon_fontify'
+	def return_emoticon(content)
+		EmoticonFontify.emoticon_fontify(h(content))
+	end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -9,8 +9,12 @@ class User < ActiveRecord::Base
 	before_save { self.email = email.downcase }
 	before_create :create_remember_token
 	validates :name, presence: true, length: { maximum: 50 }
-	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  	validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness:  { case_sensitive: false }
+	# VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]*[a-z\d]+\.[a-z]+\z/i
+	
+	VALID_EMAIL_REGEX = /\A[\w+\-.]+@([a-zA-Z0-9-]+\.)+[a-zA-Z]+\z/i
+	#VALID_ME_REGEX = /\A[a-z\d\-]+\.[\.]+[a-z\d\-]+\z/i
+	# VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.][a-z\d\-]+\.[a-z]+\z/i
+  	validates :email, presence: true, format: { with: VALID_EMAIL_REGEX}, uniqueness:  { case_sensitive: false }
   	validates :password, length: {minimum: 6}
   	has_secure_password
 
